4da98843aa0ba18adbad1561f49adc098c32d406
1buff <113794589+1buff@users.noreply.github.com>
2022-10-30 18:23:03 +0000

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,29 @@
+module Go_torring
+
+go 1.19
+
+require github.com/imroc/req/v3 v3.25.0
+
+require (
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/google/pprof v0.0.0-20221010195024-131d412537ea // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/lucas-clemente/quic-go v0.30.0 // indirect
+	github.com/marten-seemann/qpack v0.3.0 // indirect
+	github.com/marten-seemann/qtls-go1-16 v0.1.5 // indirect
+	github.com/marten-seemann/qtls-go1-17 v0.1.2 // indirect
+	github.com/marten-seemann/qtls-go1-18 v0.1.3 // indirect
+	github.com/marten-seemann/qtls-go1-19 v0.1.1 // indirect
+	github.com/onsi/ginkgo/v2 v2.4.0 // indirect
+	github.com/simonnilsson/ask v0.3.0 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/exp v0.0.0-20221026153819-32f3d567a233 // indirect
+	golang.org/x/mod v0.6.0 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/tools v0.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/imroc/req/v3"
+	"github.com/simonnilsson/ask"
+)
+
+func check_bridge(bridge, fingerprint string, bridge_chan chan string) {
+	// fmt.Printf("Checking %s\n", bridge)
+	conn, err := net.DialTimeout("tcp", bridge, 500*time.Millisecond)
+	if err == nil {
+		bridge_chan <- fmt.Sprintf("Bridge %s %s", bridge, fingerprint)
+		conn.Close()
+	}
+
+}
+
+func get_bridges_and_check() {
+	var response map[string]interface{}
+	var working_bridges = []string{}
+	bridges_chan := make(chan string)
+
+	body, err := req.Get("https://onionoo.torproject.org/details?type=relay&running=true&fields=fingerprint,or_addresses")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.Unmarshal(body.Bytes(), &response)
+
+	go func() {
+		for i := 0; ; i++ {
+			select {
+			case working_bridge := <-bridges_chan:
+				if i >= 5 {
+					for _, j := range working_bridges {
+						fmt.Println(j)
+					}
+					fmt.Println("UseBridges 1")
+					os.Exit(0)
+				}
+				working_bridges = append(working_bridges, working_bridge)
+			}
+		}
+	}()
+
+	for i := 0; ; i++ {
+		address := ask.For(response, fmt.Sprintf("relays[%d].or_addresses[0]", i)).Value()
+		fingerprint := ask.For(response, fmt.Sprintf("relays[%d].fingerprint", i)).Value()
+
+		go check_bridge(address.(string), fingerprint.(string), bridges_chan)
+
+		time.Sleep(50 * time.Millisecond)
+	}
+
+}
+func main() {
+	get_bridges_and_check()
+}
